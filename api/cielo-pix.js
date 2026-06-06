beaa4948551058af7e17c007e4b55590d9893305
isacjeferson9-afk <isac.jeferson.9@gmail.com>
2026-06-06 00:43:12 -0300

    Update cielo-pix.js

--- api/cielo-pix.js
+++ api/cielo-pix.js
@@ -1,1 +1,8 @@
-
+export default async function handler(req, res) {
+  res.status(200).json({
+    status: "ok",
+    mensagem: "Arquivo cielo-pix funcionando",
+    merchantIdExiste: !!process.env.CIELO_MERCHANT_ID,
+    merchantKeyExiste: !!process.env.CIELO_MERCHANT_KEY
+  });
+}
