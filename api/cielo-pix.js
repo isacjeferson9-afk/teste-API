27e7a9ccacaf1d1fff7e62df21d0c07f2110e069
isacjeferson9-afk <isac.jeferson.9@gmail.com>
2026-06-06 00:53:59 -0300

    Update cielo-pix.js

--- api/cielo-pix.js
+++ api/cielo-pix.js
@@ -1,8 +1,39 @@
 export default async function handler(req, res) {
+  const resposta = await fetch(
+    "https://apisandbox.cieloecommerce.cielo.com.br/1/sales",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "MerchantId": process.env.CIELO_MERCHANT_ID,
+        "MerchantKey": process.env.CIELO_MERCHANT_KEY
+      },
+      body: JSON.stringify({
+        MerchantOrderId: "TESTE001",
+        Customer: {
+          Name: "Cliente Teste"
+        },
+        Payment: {
+          Type: "CreditCard",
+          Amount: 100,
+          Installments: 1,
+          SoftDescriptor: "TESTE",
+          CreditCard: {
+            CardNumber: "0000000000000001",
+            Holder: "Teste",
+            ExpirationDate: "12/2030",
+            SecurityCode: "123",
+            Brand: "Visa"
+          }
+        }
+      })
+    }
+  );
+
+  const texto = await resposta.text();
+
   return res.status(200).json({
-    merchantId: process.env.CIELO_MERCHANT_ID?.substring(0, 8),
-    merchantKeyTamanho: process.env.CIELO_MERCHANT_KEY?.length,
-    merchantIdExiste: !!process.env.CIELO_MERCHANT_ID,
-    merchantKeyExiste: !!process.env.CIELO_MERCHANT_KEY
+    status: resposta.status,
+    resposta: texto
   });
 }
